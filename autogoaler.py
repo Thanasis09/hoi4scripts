0fa4f6f3138097e633ece90565256e3d653ff5b9
Thanasis09 <57492658+Thanasis09@users.noreply.github.com>
2024-01-17 20:24:20 +0200

    fixes

--- autogoaler.py
+++ autogoaler.py
@@ -24,6 +24,8 @@ import os
 ### If it does exist, it will create a goals.gfx entry using the filename
 ### then ask for another one
 ### if you want to exit the script instead, write exit.
+### The script is also specifically written to support FX files, 
+### though it is very easy to change the paths if you want to.
 
 #############################################################
 
@@ -36,36 +38,36 @@ while True:
 
     if filename.lower() == 'exit':
         break
-    
+
     matching_files = [f for f in os.listdir(goal_directory) if f == filename]
-    
+
     if not matching_files:
         print(f"File '{filename}' not found in the 'gfx/interface/goals' directory.")
         continue
-    
+
     interface_directory = os.path.join(current_directory, "interface")
     fx_goals_filepath = os.path.join(interface_directory, "FX_goals.gfx")
-    
-    with open(fx_goals_filepath, 'a') as fx_goals_file:
+
+    with open(fx_goals_filepath, 'a', encoding="utf-8") as fx_goals_file:
         # Step 6: Find the last closing bracket of the spriteTypes block
         last_bracket = None
-        with open(fx_goals_filepath, 'r') as fx_goals_read:
+        with open(fx_goals_filepath, 'r', encoding="utf-8") as fx_goals_read:
             lines = fx_goals_read.readlines()
             for index, line in enumerate(reversed(lines)):
                 if "}" in line:
                     last_bracket = len(lines) - index - 1
                     break
-        
+
         if last_bracket is not None:
-            
+
             new_filename = os.path.join("gfx/interface/goals", filename).replace("\\", "/")
             new_sprite_type = f'\n\tspriteType = {{\n\t\tname = "GFX_goal_{sprite_id}"\n\t\ttextureFile = "{new_filename}"\n\t}}'
 
             lines.insert(last_bracket, new_sprite_type + "\n")
-            
-            with open(fx_goals_filepath, 'w') as fx_goals_rewrite:
+
+            with open(fx_goals_filepath, 'w', encoding="utf-8") as fx_goals_rewrite:
                 fx_goals_rewrite.writelines(lines)
-                
+
             print(f"New SpriteType entry added for '{filename}'")
         else:
             print("No closing bracket found for spriteTypes block in 'FX_goals.gfx'")
--- ddsToPng.py
+++ ddsToPng.py
@@ -1,24 +1,28 @@
-from PIL import Image
 import os
+from PIL import Image
 
 #############################################################
 
 ### usage: ddsToPng.py
 ###
-### The script does not use argparse or another utility to parse the files, and instead relies that you put the paths manually.
+### The script does not use argparse or another utility to parse the files, 
+### and instead relies that you put the paths manually.
 ### This may be changed soon to support command-line arguments.
 ### The script uses pillow module, which may not be installed on your machine. To install, run 
 ### pip install pillow
 ###
-### Using an input folder and an output folder, it converts all files on input folder to png from dds.
-### Warning: It does not change the spriteType entries, you have to do that manually possibly using find all + replace.
-### The script is meant for those that want to convert all their files to png from dds, as dds is a format that is really quirky
+### Using an input folder and an output folder, 
+### it converts all files on input folder to png from dds.
+### Warning: It does not change the spriteType entries, 
+### you have to do that manually possibly using find all + replace.
+### The script is meant for those that want to convert all their files to png from dds, 
+### as dds is a format that is really quirky
 ### and hoi4 does support png.
 
 def convert_to_png(input_folder, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-        
+
     for root, _, filenames in os.walk(input_folder):
         for filename in filenames:
             name, ext = os.path.splitext(filename)
@@ -32,7 +36,7 @@ def convert_to_png(input_folder, output_folder):
                     print(f"Converted: {input_path} -> {output_path}")
                 except Exception as e:
                     print(f"Error converting {input_path}: {e}")
-                    
+
 def main():
     input_folder = "gfx"
     output_folder = "gfx/converted/"
